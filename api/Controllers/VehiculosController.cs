3629c3c69360b7ee7e9b46387f0d944e15c61a3d
Matias Martinez <mmartinezmaldonado@entaconsulting.com>
2024-07-06 15:31:00 -0300

    Cambios en el controlador de vehiculos

--- api/Controllers/VehiculosController.cs
+++ api/Controllers/VehiculosController.cs
@@ -1,18 +1,16 @@
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using api.DataAccess;
-using api.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
-using YourNamespace;
 
 namespace api.Controllers;
 
-[Route("api/vehiculos")]
+[Route("api/[controller]")]
 [ApiController]
 public class VehiculosController : ControllerBase
 {
+    // TODO: Definir permisos
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly IRdaUnitOfWork _unitOfWork;
 
@@ -22,61 +20,95 @@ public class VehiculosController : ControllerBase
         _httpClientFactory = httpClientFactory;
     }
     
-    [HttpGet("GetVehiculos")]
-    public IActionResult GetVehiculos()
+    [HttpGet]
+    public async Task<IActionResult> GetVehiculos()
     {
         var httpClient = _httpClientFactory.CreateClient("CrmHttpClient");
 
-        var response = httpClient.GetAsync("crm/v2/Vehiculos").Result;
-        var json = response.Content.ReadAsStringAsync().Result;
+        var uri = new StringBuilder("crm/v2/Vehiculos");
+
+        // TODO: Filtrar los contratos segun el rol del usuario.
+        // var empresa = string.Empty;
+
+        // uri.Append(false ? $"/search?criteria=(Owner.Name:equals:{empresa})&" : "?");
+
+        // uri.Append("fields=id,Estado,Marca_Vehiculo,Modelo,Versi_n,Chasis,Color,A_o,Pa_s,Medida_Cubierta,Fecha_de_patentamiento,"+
+        //     "Od_metro,Poliza_Seguro,Compa_a_de_seguro,Tipo_cobertura,Franquicia,Poliza_N,Fecha_siguiente_VTV,Vencimiento_Matafuego,"+
+        //     "Vencimiento_de_Ruta,Padron,Vto_Cedula_verde,Owner");
+
+        var response = await httpClient.GetAsync(uri.ToString());
+        var json = await response.Content.ReadAsStringAsync();
 
         return Ok(json);
     }
 
-    [HttpPost("ActualizarVehiculo")]
-    public IActionResult ActualizarVehiculo([FromRoute] int id, [FromBody]VehiculoDto vehiculo)
+    [HttpPost("{id}/AsignarConductor")]
+    public async Task<IActionResult> AsignarConductor(int id, [FromBody] string conductor)
     {
-        //TDDO pueden modificar cualquier campo? Qué campos si y qué campos no? Son todos los del CRM?
-        
-        var jsonContent = JsonSerializer.Serialize(vehiculo, new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
-            });
+        var jsonContent = JsonSerializer.Serialize(new { conductor = conductor }, new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        });
 
         var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
         var httpClient = _httpClientFactory.CreateClient("CrmHttpClient");
 
-        var response = httpClient.PatchAsync("crm/v2/Vehiculos/" + id, content).Result;
+        // Deberiamos consultar en que tabla se relacionan los conductores y los vehiculos.
+        // Potencialmente parece ser "Vehiculos-drivers".
+
+        // var response = await httpClient.PatchAsync("crm/v2/Vehiculos/" + id, content);
 
         return Ok();
     }
 
-    [HttpDelete("EliminarVehiculo")]
-    public void EliminarVehiculo([FromRoute] int id)
-    {
-        var httpClient = _httpClientFactory.CreateClient("CrmHttpClient");
+    // Segun funcional estos ep no son necesarios. Los dejo comentado porque el discovery decia algo diferente.
 
-        var response = httpClient.DeleteAsync("crm/v2/Vehiculos/" + id).Result;
-    }
+    // [HttpPost("ActualizarVehiculo")]
+    // public IActionResult ActualizarVehiculo([FromRoute] int id, [FromBody] VehiculoDto vehiculo)
+    // {
+    //     //TDDO pueden modificar cualquier campo? Qué campos si y qué campos no? Son todos los del CRM?
 
-    [HttpPost("CrearVehiculo")]
-    public IActionResult CrearVehiculo([FromRoute] CrearVehiculoDto newVehiculo)
-    {
-        //TODO que campos son requeridos? Segun el CRM solamente el nombre. Esto es asi? Que definieron?
-        var httpClient = _httpClientFactory.CreateClient("CrmHttpClient");
+    //     var jsonContent = JsonSerializer.Serialize(vehiculo, new JsonSerializerOptions
+    //     {
+    //         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    //         DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+    //     });
 
-        var options = new JsonSerializerOptions
-        {
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-        };
+    //     var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
-        var json = new StringContent(JsonSerializer.Serialize(newVehiculo, options), Encoding.UTF8, "application/json");
+    //     var httpClient = _httpClientFactory.CreateClient("CrmHttpClient");
 
-        var response = httpClient.PostAsync("crm/v2/Vehiculos", json).Result;
+    //     var response = httpClient.PatchAsync("crm/v2/Vehiculos/" + id, content).Result;
 
-        var responseJson = response.Content.ReadAsStringAsync().Result;
-        return Ok(responseJson);
-    }
+    //     return Ok();
+    // }
+
+    // [HttpDelete("EliminarVehiculo")] 
+    // public void EliminarVehiculo([FromRoute] int id)
+    // {
+    //     var httpClient = _httpClientFactory.CreateClient("CrmHttpClient");
+
+    //     var response = httpClient.DeleteAsync("crm/v2/Vehiculos/" + id).Result;
+    // }
+
+    // [HttpPost("CrearVehiculo")]
+    // public IActionResult CrearVehiculo([FromRoute] CrearVehiculoDto newVehiculo)
+    // {
+    //     //TODO que campos son requeridos? Segun el CRM solamente el nombre. Esto es asi? Que definieron?
+    //     var httpClient = _httpClientFactory.CreateClient("CrmHttpClient");
+
+    //     var options = new JsonSerializerOptions
+    //     {
+    //         DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    //     };
+
+    //     var json = new StringContent(JsonSerializer.Serialize(newVehiculo, options), Encoding.UTF8, "application/json");
+
+    //     var response = httpClient.PostAsync("crm/v2/Vehiculos", json).Result;
+
+    //     var responseJson = response.Content.ReadAsStringAsync().Result;
+    //     return Ok(responseJson);
+    // }
 }
