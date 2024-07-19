e5b46b76e5f4827d37f1114449c04fb6124145c0
Matias Ezequiel Smith <msmith@entaconsulting.com>
2024-07-18 21:35:12 -0300

    Remove campo EmpresaId de tabla User

---
+++ api/Migrations/20240719003406_usuarios_deleteEmpresaId.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class usuarios_deleteEmpresaId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Empresas_Empresaid",
+                table: "User");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_Empresaid",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Empresaid",
+                table: "User");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Empresaid",
+                table: "User",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Empresaid",
+                table: "User",
+                column: "Empresaid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Empresas_Empresaid",
+                table: "User",
+                column: "Empresaid",
+                principalTable: "Empresas",
+                principalColumn: "id");
+        }
+    }
+}
--- api/Models/Entities/Empresa.cs
+++ api/Models/Entities/Empresa.cs
@@ -11,7 +11,6 @@ namespace api.Models.Entities
         public string? razonSocial { get; set; }
         public Guid guid { get; set;}
         public string? idCRM { get; set; }
-        public virtual ICollection<User>? Usuarios { get; set; }
         public virtual ICollection<UsuariosEmpresas>? Asignaciones { get; set; }
     }
 }
