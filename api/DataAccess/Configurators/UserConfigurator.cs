b9a977b2bd5b566a359b98bd8eb1c5980d9c0ff1
BraianAzcune <braian.s.azcune@hotmail.com>
2024-07-25 17:48:43 -0300

    conductor puede editar su propio numero de telefono

--- api/DataAccess/Configurators/UserConfigurator.cs
+++ api/DataAccess/Configurators/UserConfigurator.cs
@@ -7,69 +7,68 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
-namespace api.DataAccess.Configurators
+namespace api.DataAccess.Configurators;
+
+public class UserConfigurator
 {
-    public class UserConfigurator
+    public void Configure(EntityTypeBuilder<User> builder)
     {
-        public void Configure(EntityTypeBuilder<User> builder)
-        {
-            builder.ToTable("Users");
-            builder.HasKey(x => x.id);
+        builder.ToTable("Users");
+        builder.HasKey(x => x.id);
 
-            builder.Property(x => x.id).IsRequired().HasColumnName("id").HasColumnType("int");
-            builder
-                .Property(x => x.userName)
-                .IsRequired()
-                .HasColumnName("userName")
-                .HasColumnType("nvarchar")
-                .HasMaxLength(100);
-            builder
-                .Property(x => x.nombre)
-                .IsRequired()
-                .HasColumnName("nombre")
-                .HasColumnType("nvarchar")
-                .HasMaxLength(100);
-            builder
-                .Property(x => x.apellido)
-                .IsRequired()
-                .HasColumnName("apellido")
-                .HasColumnType("nvarchar")
-                .HasMaxLength(100);
-            builder
-                .Property(x => x.estado)
-                .IsRequired()
-                .HasColumnName("estado")
-                .HasColumnType("nvarchar")
-                .HasMaxLength(200);
-            builder
-                .Property(x => x.guid)
-                .IsRequired()
-                .HasColumnName("guid")
-                .HasColumnType("uniqueidentifier")
-                .IsRequired();
-            builder
-                .Property(x => x.isRDA)
-                .IsRequired()
-                .HasColumnName("isRDA")
-                .HasColumnType("bit")
-                .HasDefaultValue(1);
-            builder
-                .Property(x => x.idCRM)
-                .IsRequired()
-                .HasColumnName("idCRM")
-                .HasColumnType("nvarchar")
-                .HasMaxLength(100);
-            builder
-                .Property(x => x.telefono)
-                .HasColumnName("telefono")
-                .HasColumnType("nvarchar")
-                .HasMaxLength(100);
+        builder.Property(x => x.id).IsRequired().HasColumnName("id").HasColumnType("int");
+        builder
+            .Property(x => x.userName)
+            .IsRequired()
+            .HasColumnName("userName")
+            .HasColumnType("nvarchar")
+            .HasMaxLength(100);
+        builder
+            .Property(x => x.nombre)
+            .IsRequired()
+            .HasColumnName("nombre")
+            .HasColumnType("nvarchar")
+            .HasMaxLength(100);
+        builder
+            .Property(x => x.apellido)
+            .IsRequired()
+            .HasColumnName("apellido")
+            .HasColumnType("nvarchar")
+            .HasMaxLength(100);
+        builder
+            .Property(x => x.estado)
+            .IsRequired()
+            .HasColumnName("estado")
+            .HasColumnType("nvarchar")
+            .HasMaxLength(200);
+        builder
+            .Property(x => x.guid)
+            .IsRequired()
+            .HasColumnName("guid")
+            .HasColumnType("uniqueidentifier")
+            .IsRequired();
+        builder
+            .Property(x => x.isRDA)
+            .IsRequired()
+            .HasColumnName("isRDA")
+            .HasColumnType("bit")
+            .HasDefaultValue(1);
+        builder
+            .Property(x => x.idCRM)
+            .IsRequired()
+            .HasColumnName("idCRM")
+            .HasColumnType("nvarchar")
+            .HasMaxLength(100);
+        builder
+            .Property(x => x.telefono)
+            .HasColumnName("telefono")
+            .HasColumnType("nvarchar")
+            .HasMaxLength(100);
 
-            //Relaciones
-            builder
-                .HasMany(x => x.EmpresasAsignaciones)
-                .WithOne(u => u.User)
-                .HasForeignKey(q => q.userId);
-        }
+        //Relaciones
+        builder
+            .HasMany(x => x.EmpresasAsignaciones)
+            .WithOne(u => u.User)
+            .HasForeignKey(q => q.userId);
     }
 }
---
+++ api/Models/DTO/Conductor/UpdateSelfConductorDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Models.DTO.Conductor;
+
+public class UpdateSelfConductorDto
+{
+    [Phone(ErrorMessage = "El número de teléfono no es válido")]
+    public required string Telefono { get; set; }
+}
