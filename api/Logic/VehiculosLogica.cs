3b001e4a4190660eff5f72868a0d397259d4a874
Matias Martinez <mmartinezmaldonado@entaconsulting.com>
2024-07-26 13:22:38 -0300

    #69-Pasando la logica al servicio de Vehiculos

--- api/Logic/VehiculosLogica.cs
+++ api/Logic/VehiculosLogica.cs
@@ -1,11 +1,3 @@
-using System.Text;
-using System.Text.Json;
-using api.Connected_Services;
-using api.Models.DTO;
-using api.Models.DTO.Operaciones;
-using api.Models.DTO.Vehiculo;
-using Newtonsoft.Json.Linq;
-
 namespace api.Logic
 {
     public class VehiculosLogica { }
--- api/Services/IVehiculoService.cs
+++ api/Services/IVehiculoService.cs
@@ -1,4 +1,6 @@
+using System.Security.Claims;
 using api.Models.DTO;
+using api.Models.DTO.Operaciones;
 using api.Models.DTO.Vehiculo;
 
 namespace api.Services
@@ -6,6 +8,8 @@ namespace api.Services
     public interface IVehiculoService
     {
         Task<string?> AsignarVehiculo(AsignarVehiculoDto asignarVehiculoDto);
-        Task<List<VehiculoDto>?> GetVehiculos(System.Security.Claims.ClaimsPrincipal User);
+        Task<List<VehiculoDto>?> GetVehiculos(ClaimsPrincipal User);
+        Task<List<OperacionesVehiculoDto>> HistorialOperaciones(ClaimsPrincipal User,
+            string dominio, string tipoContrato);
     }
 }
