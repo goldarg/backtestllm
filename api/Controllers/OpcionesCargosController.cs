7d9736850678700ada80d1642953392a0bf52e2e
BraianAzcune <braian.s.azcune@hotmail.com>
2024-07-29 15:29:49 -0300

    #120 agregar y eliminar OpcionesCargos

--- api/Controllers/OpcionesCargosController.cs
+++ api/Controllers/OpcionesCargosController.cs
@@ -19,4 +19,35 @@ public class OpcionesCargosController(IRdaUnitOfWork unitOfWork) : ControllerBas
             .ToList();
         return Ok(puestos);
     }
+
+    [HttpPost]
+    public IActionResult PostPuesto([FromBody] string puesto)
+    {
+        // check que no se repita
+        var isPuestoExists = unitOfWork
+            .GetRepository<OpcionesCargos>()
+            .GetAll()
+            .Any(x => x.Nombre == puesto);
+        if (isPuestoExists)
+            return BadRequest("El puesto ya existe");
+        var puestoEntity = new OpcionesCargos { Nombre = puesto };
+        unitOfWork.GetRepository<OpcionesCargos>().Insert(puestoEntity);
+        unitOfWork.SaveChanges();
+        return Ok();
+    }
+
+    // borrar recibe el nombre
+    [HttpDelete]
+    public IActionResult DeletePuesto([FromBody] string puesto)
+    {
+        var puestoEntity = unitOfWork
+            .GetRepository<OpcionesCargos>()
+            .GetAll()
+            .FirstOrDefault(x => x.Nombre == puesto);
+        if (puestoEntity == null)
+            return BadRequest("El puesto no existe");
+        unitOfWork.GetRepository<OpcionesCargos>().Delete(puestoEntity);
+        unitOfWork.SaveChanges();
+        return Ok();
+    }
 }
